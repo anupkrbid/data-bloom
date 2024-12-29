320cfa8b1a51882502590893db47d5f4b1b96047
Anup Kumar Bid <anup@spotdraft.com>
2024-12-29 06:26:00 +0530

    dashboard layout fixed

--- react-frontend/src/pages/Dashboard/Dashboard.jsx
+++ react-frontend/src/pages/Dashboard/Dashboard.jsx
@@ -5,40 +5,38 @@ import Stack from '@mui/material/Stack';
 import AppNavbar from '../../layouts/AppNavBar/AppNavBar';
 import Header from '../../layouts/Header/Header';
 import MainGrid from '../../components/features/Dashboard/MainGrid/MainGrid';
-
+import { MuiFlexContainer } from '../../components/common';
 
 export default function Dashboard() {
   return (
-      <Box sx={{ display: 'flex' }}>
-        <AppNavbar />
-        <Box
-          component="main"
-          sx={(theme) => ({
-            flexGrow: 1,
-            backgroundColor: theme.vars
-              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-              : alpha(theme.palette.background.default, 1),
-            overflow: 'auto',
-          })}
+    <MuiFlexContainer direction="column">
+      <AppNavbar />
+      <Box
+        component="main"
+        sx={(theme) => ({
+          flexGrow: 1,
+          backgroundColor: theme.vars
+            ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+            : alpha(theme.palette.background.default, 1),
+          overflow: 'auto'
+        })}
+      >
+        <Stack
+          spacing={2}
+          sx={{
+            alignItems: 'center',
+            mx: 3,
+            pb: 5,
+            mt: 8
+          }}
         >
-          <Stack
-            spacing={2}
-            sx={{
-              alignItems: 'center',
-              mx: 3,
-              pb: 5,
-              mt: 8,
-            }}
-          >
-            <Header />
-            <MainGrid />
-          </Stack>
-        </Box>
+          <Header />
+          <MainGrid />
+        </Stack>
       </Box>
-  
+    </MuiFlexContainer>
   );
 }
-
 
 // import { useLoaderData } from 'react-router-dom';
 
